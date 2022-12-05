3abf1f3cd145370f31a3a78ae72da7adbbcdb477
Brett Coomer <brett.coomer@gmail.com>
2022-12-05 09:26:15 +0100

    feat: random and debug launch

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/brettc-84/advent-of-code-2022/day01"
 	"github.com/brettc-84/advent-of-code-2022/day02"
 	"github.com/brettc-84/advent-of-code-2022/day03"
 	"github.com/brettc-84/advent-of-code-2022/day04"
+	"github.com/brettc-84/advent-of-code-2022/day05"
 	"github.com/brettc-84/advent-of-code-2022/utils"
 )
 
@@ -30,11 +32,14 @@ func main() {
 	fmt.Println("***** AoC 2022 *****")
 	fmt.Println("***** Lets GOOO *****")
 
+	seed := rand.NewSource((time.Now().UnixNano()))
+	random := rand.New(seed)
 	challenges := []challenge{
-		{1, d1, colours[rand.Intn(len(colours))]},
-		{2, d2, colours[rand.Intn(len(colours))]},
-		{3, d3, colours[rand.Intn(len(colours))]},
-		{4, d4, colours[rand.Intn(len(colours))]},
+		{1, d1, colours[random.Intn(len(colours))]},
+		{2, d2, colours[random.Intn(len(colours))]},
+		{3, d3, colours[random.Intn(len(colours))]},
+		{4, d4, colours[random.Intn(len(colours))]},
+		{5, d5, colours[random.Intn(len(colours))]},
 	}
 
 	for _, dayChallenge := range challenges {
@@ -67,5 +72,10 @@ func d4() {
 	input := utils.ReadMultiLine("./day04/input.txt")
 	fmt.Println("Part1:", day04.Part1(input))
 	fmt.Println("Part2:", day04.Part2(input))
+}
 
+func d5() {
+	input := utils.ReadMultiLine("./day05/input.txt")
+	fmt.Println("Part1:", day05.Part1(input))
+	fmt.Println("Part2:", day05.Part2(input))
 }
